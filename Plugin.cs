c96c31e9ad471cec18df542bdbb2040b66809ed3
Кирилл <131317714+Morkamo@users.noreply.github.com>
2025-11-10 20:19:11 +0900

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+
+namespace AdrenalinePlus
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name => "AdrenalinePlus";
+        public override string Prefix => Name;
+        public override string Author => "Morkamo";
+        public override Version RequiredExiledVersion => new(9, 1, 0);
+        public override Version Version => new(0, 0, 1);
+        public override PluginPriority Priority => PluginPriority.Lowest;
+
+        public static Plugin Instance;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Instance = null;
+            base.OnDisabled();
+        }
+    }
+}
